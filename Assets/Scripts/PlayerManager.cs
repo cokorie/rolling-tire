ad9975c16cac815eb7f953c85c7d316c13acb28c
cokorie <chijioke.okorie@gmail.com>
2023-02-16 11:15:19 -0600

    Add Game Over Screen

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public static bool gameOver;
+    public GameObject gameOverPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameOver)
+        {
+            Time.timeScale = 0;
+            gameOverPanel.SetActive(true);
+        }
     }
 }
